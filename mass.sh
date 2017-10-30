7eca3b7f2cbfeb726740c48ef6a942e9070d20bf
Jason Haddix <j.haddix56@gmail.com>
2017-10-30 13:15:44 -0700

    Update mass.sh

--- mass.sh
+++ mass.sh
@@ -6,4 +6,3 @@ host $strip
 echo "##################################################"
 echo ""
 masscan -p1-65535 $(dig +short $strip|grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"|head -1) --max-rate 1000 |& tee $strip_scan
-#cat $1_scan |grep Discovered |sort -u | cut -d " " -f4|cut -d / -f1
